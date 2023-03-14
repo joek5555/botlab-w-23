3dcf2f23edfadc4a6db2c39ab638199c810040bc
Joseph Kennedy <joekennedy5555@gmail.com>
2023-03-14 15:24:32 -0400

    Change ramp to lower speeds

--- scripts/python/mbot_ramp_up_speed.py
+++ scripts/python/mbot_ramp_up_speed.py
@@ -10,18 +10,22 @@ turn_vel = 3.14 # pi rad/sec
 lc = lcm.LCM("udpm://239.255.76.67:7667?ttl=1")
 time.sleep(0.5)
 command = mbot_motor_command_t()
+command.trans_v = 0
+command.angular_v = 0
+lc.publish("MBOT_MOTOR_COMMAND",command.encode())
+time.sleep(1)
 
 
-for i in range (100):
-    command.trans_v = 0.01 * i
+for i in range (20):
+    command.trans_v = 0.001 * i
     command.angular_v = 0
     lc.publish("MBOT_MOTOR_COMMAND",command.encode())
     print( command.trans_v)
-    time.sleep(0.5)
+    time.sleep(1)
 
-for i in range (100):
-    command.trans_v = 1 - 0.01 * i
+for i in range (20):
+    command.trans_v = 0.02 - 0.001 * i
     command.angular_v = 0
     lc.publish("MBOT_MOTOR_COMMAND",command.encode())
     print( command.trans_v)
-    time.sleep(0.5)
+    time.sleep(1)
