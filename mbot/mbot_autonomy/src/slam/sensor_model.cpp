2c07d5961819b2c084a3f38a914b90b8bcd82716
Joseph Kennedy <josephpk@umich.edu>
2023-04-02 22:53:27 -0400

    Add way to increase scan score if there is an obstacle close to laser endpoint

--- mbot/mbot_autonomy/src/slam/sensor_model.cpp
+++ mbot/mbot_autonomy/src/slam/sensor_model.cpp
@@ -13,17 +13,52 @@ double SensorModel::likelihood(const mbot_lcm_msgs::particle_t& sample,
                                const mbot_lcm_msgs::lidar_t& scan, 
                                const OccupancyGrid& map)
 {
-    double likelihood = 0.0;
+    double likelihood = 1.0;
     MovingLaserScan movingScan(scan, sample.parent_pose, sample.pose, ray_stride_);
+    int i = 0;
 
     for (auto& ray : movingScan){
-        Point<double> endpt(ray.origin.x + ray.range * std::cos(ray.theta),
-                            ray.origin.y + ray.range * std::sin(ray.theta));
-        auto rayEndPt = global_position_to_grid_cell(endpoint, map);
-        if (map.logOdds(rayEndPt.x, rayEndPt.y) > 0.0){
-            likelihood += 1.0;
+        i++;
+        if(ray.range < max_lidar_range){
+            Point<double> endpt(ray.origin.x + ray.range * std::cos(ray.theta),
+                                ray.origin.y + ray.range * std::sin(ray.theta));
+            auto rayEndPt = global_position_to_grid_cell(endpt, map);
+            if (map.logOdds(rayEndPt.x, rayEndPt.y) > 0.0){
+                likelihood += 100.0;
+                std::cout << i << " ray entered first if statement" << std::endl;
+            }
+            else if (map.logOdds(rayEndPt.x+1, rayEndPt.y) > 0.0 ||
+                     map.logOdds(rayEndPt.x+1, rayEndPt.y+1) > 0.0 || 
+                     map.logOdds(rayEndPt.x, rayEndPt.y+1) > 0.0 ||
+                     map.logOdds(rayEndPt.x-1, rayEndPt.y+1) > 0.0 ||
+                     map.logOdds(rayEndPt.x-1, rayEndPt.y) > 0.0 ||
+                     map.logOdds(rayEndPt.x-1, rayEndPt.y-1) > 0.0 ||
+                     map.logOdds(rayEndPt.x, rayEndPt.y-1) > 0.0 ||
+                     map.logOdds(rayEndPt.x+1, rayEndPt.y-1) > 0.0){
+                likelihood += 75.0;
+                std::cout << i << " ray entered second if statement" << std::endl;
+            }
+            else if (map.logOdds(rayEndPt.x+2, rayEndPt.y) > 0.0 ||
+                     map.logOdds(rayEndPt.x+2, rayEndPt.y+1) > 0.0 || 
+                     map.logOdds(rayEndPt.x+2, rayEndPt.y+2) > 0.0 ||
+                     map.logOdds(rayEndPt.x+1, rayEndPt.y+2) > 0.0 ||
+                     map.logOdds(rayEndPt.x, rayEndPt.y+2) > 0.0 ||
+                     map.logOdds(rayEndPt.x-1, rayEndPt.y+2) > 0.0 ||
+                     map.logOdds(rayEndPt.x-2, rayEndPt.y+2) > 0.0 ||
+                     map.logOdds(rayEndPt.x-2, rayEndPt.y+1) > 0.0 || 
+                     map.logOdds(rayEndPt.x-2, rayEndPt.y) > 0.0 ||
+                     map.logOdds(rayEndPt.x-2, rayEndPt.y-1) > 0.0 ||
+                     map.logOdds(rayEndPt.x-2, rayEndPt.y-2) > 0.0 ||
+                     map.logOdds(rayEndPt.x-1, rayEndPt.y-2) > 0.0 ||
+                     map.logOdds(rayEndPt.x, rayEndPt.y-2) > 0.0 ||
+                     map.logOdds(rayEndPt.x+1, rayEndPt.y-2) > 0.0 ||
+                     map.logOdds(rayEndPt.x+2, rayEndPt.y-2) > 0.0 ||
+                     map.logOdds(rayEndPt.x+2, rayEndPt.y-1) > 0.0){
+                likelihood += 50.0;
+                std::cout << i << " ray entered third if statement" << std::endl;
+            }
         }
     }
     // TODO
     return likelihood;
-}
+} 
